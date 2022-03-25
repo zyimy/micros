27d2453dfcf8b3ddac8e69127662d32006ed522e
Yimy Zepeda <zepedayimy45@gmail.com>
2022-03-25 11:41:56 +0100

    inicio

---
+++ microservicios/src/main/java/com/servicios/entity/User.java
@@ -0,0 +1,10 @@
+package com.servicios.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+}
---
+++ microservicios/src/test/java/com/servicios/MicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
