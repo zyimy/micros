3cf8aa99a763753616ffa6ad33775a45f9c187e6
Yimy Zepeda <zepedayimy45@gmail.com>
2022-03-25 12:22:08 +0100

    prueba

--- microservicios/src/main/java/com/servicios/entity/User.java
+++ microservicios/src/main/java/com/servicios/entity/User.java
@@ -1,10 +1,31 @@
 package com.servicios.entity;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.criterion.NotNullExpression;
+
+
 
 @Entity
 @Table(name = "tbl_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id_user;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	private String email;
+	
+	
+	
 
 }
