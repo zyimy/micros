83becf6dd54e2fc250c3a0a50db75dcb4078268b
Yimy Zepeda <zepedayimy45@gmail.com>
2022-03-26 23:40:12 +0100

    guar

---
+++ bike-service/src/main/java/com/servicios/controller/BikeController.java
@@ -0,0 +1,84 @@
+package com.servicios.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.servicios.entity.Bike;
+import com.servicios.service.BikeService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/bike")
+public class BikeController {
+	
+	@Autowired
+	private BikeService userService;
+	
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Bike>>listAll(@RequestParam(defaultValue = "0")Integer pageNo,
+			@RequestParam(defaultValue = "10")Integer pageSize,
+			@RequestParam(defaultValue = "id")String sorBy ){
+		
+		List<Bike>lista= userService.listaUser(pageNo, pageSize, sorBy);
+		
+		if (lista.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}else {
+			return ResponseEntity.ok(lista);
+		}
+		
+		
+	}
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Bike>getIdUser(@PathVariable("id")Long id){
+		Bike user = userService.getById(id);
+		
+		if (user==null) {
+			
+			return ResponseEntity.notFound().build();
+			
+		}else {
+			return ResponseEntity.ok(user);
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Bike> save(@RequestBody Bike user) {
+		Bike user2 = userService.saveUser(user);
+		
+	  return ResponseEntity.ok(user);
+			
+		}
+	
+	
+	@GetMapping("/allBike")
+	
+	public ResponseEntity<List<Bike>>listUser()
+			{
+		
+		List<Bike>lista= userService.lista();
+		
+		if (lista.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}else {
+			return ResponseEntity.ok(lista);
+		}
+		
+		
+	}
+	
+
+}
---
+++ bike-service/src/main/java/com/servicios/service/BikeService.java
@@ -0,0 +1,58 @@
+package com.servicios.service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.servicios.entity.Bike;
+import com.servicios.repository.BikeRepository;
+
+
+
+@Service
+public class BikeService {
+	
+	@Autowired
+	private BikeRepository userRepository;
+	
+	public List<Bike>listaUser(Integer pageNo,Integer pageSize,String sorBy){
+	Pageable paging = PageRequest.of(pageNo, pageSize,Sort.by(sorBy));
+	Page<Bike>pageResult= userRepository.findAll(paging);
+		
+		if (pageResult.hasContent()) {
+			return pageResult.getContent();
+		}else { 
+			return new ArrayList<Bike>();
+		}
+	}
+	
+	public Bike getById(Long id) {
+	Optional<Bike> user=userRepository.findById(id);	
+		if (user.isPresent()) {
+			return user.get();
+			
+		}else {
+			return null;
+		}
+			
+	}
+	
+	public Bike saveUser(Bike user) {
+		
+     return userRepository.save(user);    
+	
+	}
+	
+	public List<Bike>lista(){
+		return userRepository.findAll();
+	}
+
+}
