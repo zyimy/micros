9fbfad4dcacd93a63d9c19047c117e4783da9413
Yimy Zepeda <zepedayimy45@gmail.com>
2022-03-28 19:02:39 +0200

    a

--- eureka-server/src/main/java/com/servicios/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/servicios/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package com.servicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class BikeServiceApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BikeServiceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
--- microservicios/src/main/java/com/servicios/MicroserviciosApplication.java
+++ microservicios/src/main/java/com/servicios/MicroserviciosApplication.java
@@ -2,10 +2,12 @@ package com.servicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class MicroserviciosApplication {
 
 	public static void main(String[] args) {
